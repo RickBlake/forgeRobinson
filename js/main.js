b9f54e6fbb1c70aca0efd6c06986a33fc71df8d7
Unknown <richard.blake@hotmail.co.uk>
2020-04-10 14:14:17 +0100

    Fade in partials

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
-$( document ).ready( function() {
+$(document).ready(function() {
 
-    $( 'a.dynamicLoad' ).click( function( e ) {
+    $('a.dynamicLoad' ).click( function(e) {
         e.preventDefault();   // prevent the browser from following the link
         e.stopPropagation();  // prevent the browser from following the link
 
-        $( '#pageContent' ).load( $( this ).attr( 'href' ) );
+        $('#pageContent').load($(this).attr('href')).hide().fadeIn();;
 
         console.log('page loaded');
     });
 
-    $( '#pageContent' ).load('partials/home.html');
+    $('#pageContent').load('partials/home.html');
 });
