06b012e20da61093ac1a4106ba5f62de6b2f7d7e
Unknown <richard.blake@hotmail.co.uk>
2020-04-10 14:04:04 +0100

    Page content loads in partials

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 
+$( document ).ready( function() {
+
+    $( 'a.dynamicLoad' ).click( function( e ) {
+        e.preventDefault();   // prevent the browser from following the link
+        e.stopPropagation();  // prevent the browser from following the link
+
+        $( '#pageContent' ).load( $( this ).attr( 'href' ) );
+
+        console.log('page loaded');
+    });
+
+    $( '#pageContent' ).load('partials/home.html');
+});
