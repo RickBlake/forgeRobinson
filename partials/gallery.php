678d964767b32f7c4cd273778b09406a7aa9f266
Unknown <richard.blake@hotmail.co.uk>
2020-04-14 18:17:18 +0100

    Add EXIF description to images

--- partials/gallery.php
+++ partials/gallery.php
@@ -9,15 +9,20 @@
                     <?php 
                         $dir = glob('../img/gallery1/{*.jpeg, *.png}', GLOB_BRACE);
 
-                        foreach ($dir as $value) {
+                        foreach ($dir as $imageFile) {
+                            $filedata = exif_read_data($imageFile);
+                            if(is_array($filedata) && isset($filedata['ImageDescription'])){
+                                $imageDescription = $filedata['ImageDescription'];
+                            }
                     ?>
                     <div class="gallery">
                         <a target="_blank" href="">
-                            <img src="<?php echo $value; ?>" alt="Cinque Terre">
+                            <img src="<?php echo $imageFile; ?>" alt="Cinque Terre">
                         </a>
-                        <div class="desc"><?php echo $value; ?></div>
+                        <div class="desc"><?php echo $imageDescription; ?></div>
                     </div>
-                    <?php } 
+                    <?php 
+                        } 
                     ?>
 
                 </div>
