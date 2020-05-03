9695d3dd4e10c6dd16a2fd4ac36f94258b0412a7
Unknown <richard.blake@hotmail.co.uk>
2020-05-03 22:26:45 +0100

    New loading GIF

--- works/entrances.php
+++ works/entrances.php
@@ -61,17 +61,7 @@
     </div>
     </div> <!-- #main-container -->
 
-    <?php include '../../footer.php';?>
-    <script>
-        $('a.dynamicLoad').click(function (e) {
-            e.preventDefault(); // prevent the browser from following the link
-            e.stopPropagation(); // prevent the browser from following the link
-
-            $('#pageContent').load($(this).attr('href')).hide().fadeIn();;
-            $('#logo').fadeIn();
-            console.log('page loaded');
-        });
-    </script>
+    <?php include '../footer.php';?>
 
     <script>
         $('#gallery').lightGallery({
