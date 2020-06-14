76aab7e3a6af1c61ed330980f1a59057389d8b4e
Unknown <richard.blake@hotmail.co.uk>
2020-06-14 15:51:45 +0100

    Process page

--- public_html/process.php
+++ public_html/process.php
@@ -8,7 +8,7 @@
     <title>PROCESS</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <link rel="icon" href="img/favicon.png">
 
     <link rel="stylesheet" href="css/normalize.min.css">
@@ -34,21 +34,108 @@
             <div class="main wrapper clearfix">
                 <div id="pageContent">
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio
-                            egestas tempor. Nunc
-                            vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper
-                            consequat est,
-                            eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam
-                            ullamcorper lorem
-                            dapibus velit suscipit ultrices.</p>
+                    <div class="justified">
+                        <p>
+                            First produced in 1915, stainless steel contains a minimum of 10,5 % of chromium and 8 %
+                            nickel, making its resistance to corrosion phenomenal. This noble metal is now being
+                            recognized for its environmental properties too. It is 100 % recyclable. Objets made are for
+                            the long term and last many lifetimes. Roughly 60 % of stainless steel used today is of
+                            recycled material, making it a cost affective prospect. Stainless steel almost never become
+                            waste at the end of their use and the estimated end of life recycling ratio is up to 90 %.
+                        </p>
+                        <hr>
+                        <p>
+                            Produit pour la première fois en 1915, l'acier inoxydable contient un minimum de 10,5 % de
+                            chrome et 8 % de nickel, ce qui fait de lui un métal incroyablement résistant à la
+                            corrosion.
+                            De nos jours, l'inox est également reconnu pour ses propriétés environnementales, car c'est
+                            un métal 100 % recyclable. Les objets fabriqués sont faits pour durer ou servir plusieurs
+                            vies. Ils ne deviennent presque jamais du gâchis à la fin de leur utilisation, et on estime
+                            que le rapport de recyclage en fin de vie est de 90 %. Les matériaux recyclés sont à la base
+                            d'environ 60 % de l'inox qui est utilisé aujourd'hui.
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio
-                            egestas tempor. Nunc
-                            vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper
-                            consequat est,
-                            eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam
-                            ullamcorper lorem
-                            dapibus velit suscipit ultrices.</p>
+                        </p>
+                    </div>
+
+                    <div class="justified">
+
+                        <figure>
+                            <img src="img/processLeft.webp" width="100%"></img>
+                            <figcaption>
+                                Design proposal for gates with spiritual theme of peace conquering war.<br/>
+                                Proposition de dessin avec pour thème spirituel la paix conquérant la guerre.<br/>
+                                <br/>
+                                Design proposal for outdoor theatre sculptural lighting supports in inox.<br/>
+                                Proposition de dessin pour un théâtre de plein air avec supports d’éclairage
+                                sculpturaux.
+                            </figcaption>
+                        </figure>
+
+                        <p>
+                            From the outset there is a total commitment of dialogue between ourselves and the clients,
+                            architect or contractor. In certain cases, there is a budget to work within, but invariably
+                            the majority of commissions are negotiated through compromise or agreement with the design.
+
+                            Each commission is designed with either a specific client request or a complete free rein.
+                            Designs are always discussed with the client to explain the overall form and certain
+                            details, plus structural integrity and approximate construction time.
+
+                            Certainly the customer may opt for a piece in mild steel because of lower initial cost in
+                            materials, and work time, but life expectancy, even with added cost of rust prevention by
+                            galvanizing and paint, is still far less than stainless steel.
+
+                            If the work is in stainless steel, once it is cleaned, passivated and polished, either by
+                            mechanical or electro-polishing processes, it has no need for further maintenance, with the
+                            added bonus of its reflective quality of echoing its surrounding colors.
+
+                            To form many of the shapes within each work requires heating to a bright yellow and forging
+                            with the use of hand hammers and power hammers, stainless steel requiring a longer heat time
+                            and far greater force to work than mild steel.
+
+                            Other processes include cold bending of long curves, cutting and welding and finishing by
+                            grinding, sanding and polishing. On completion all work is delivered and installed, either
+                            by us or by a contractor overseen on site by us.
+
+                            Payments are usually made as a deposit, mid or interim payments and final balance after
+                            installation. On occasion, depending on the size of work, either a deposit and final payment
+                            are made or one payment on completion. Design fees are charged for projects taking longer
+                            than three weeks.
+                        </p>
+                        <hr />
+
+                        <p>
+
+                            Dès le début, il y a un engagement et un dialogue entre nous et le client, architecte ou
+                            contractant. Dans certains cas, il faut travailler avec un budget, mais les commissions sont
+                            invariablement négociés par compromis ou par accord avec le dessin.
+
+                            Chaque commission est dessinée librement ou avec la demande spécifique du client. Les
+                            dessins sont toujours sujets à une discussion avec le client, pour expliquer la forme
+                            générale et certains détails, ainsi que l'intégralité structurelle et une durée
+                            approximative de la construction.
+
+                            Bien sûr, le client peut demander pour une oeuvre en acier dû au coût initial plus bas et au
+                            temps de travail. Mais en revanche, l'espérance de vie, même avec le coût de protection
+                            antirouille par galvanisation et par peinture, reste beaucoup moins longue que l'inox.
+
+                            Si l'oeuvre est en inox, une fois qu'elle est nettoyée, passivée et polie, soit par action
+                            mécanique ou par électro-polissage, n'a plus besoin de maintien. La qualité réfléchissante
+                            et l'écho des couleurs aux alentours constituent un élément bonus.
+
+                            Une grandie partie des formes dans une oeuvre doivent être chauffées jusque'à un jaune
+                            clair, puis forgées avec des marteaux, suivi du marteau-pilon: l'inox demande un temps de
+                            chauffage plus long que l'acier doux. Nous utilisons également d'autres processus, dont la
+                            flexion de courbes longues, la soudure-découpe, et le finissage par meulage, ponçage et
+                            polissage.
+                            Une fois la réalisation achevée, toutes oeuvres sont livrés et installés, soit par nous ou
+                            par un contractant surveillé par nous sur chantier.
+
+                            Les paiements sont souvent faits sous forme de dépôt, avec les paiements mi-termes et
+                            intérimaires suite à l'installation. Parfois, en fonction de l'ampleur de l'oeuvre, soit un
+                            dépôt ou un paiement final sont faits, soit un paiement lors de l'achèvement. Les frais de
+                            dessin sont demandés pour des projets de plus de trois semaines.
+                        </p>
+                    </div>
 
                 </div>
             </div>
