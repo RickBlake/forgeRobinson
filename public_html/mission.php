416e4a820861597f09da43faa1339c156ee05107
Unknown <richard.blake@hotmail.co.uk>
2020-06-14 13:56:46 +0100

    Missing page content

--- public_html/mission.php
+++ public_html/mission.php
@@ -8,7 +8,7 @@
     <title>MISSION</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <link rel="icon" href="img/favicon.png">
 
     <link rel="stylesheet" href="css/normalize.min.css">
@@ -31,32 +31,73 @@
                         <h1>MISSION</h1>
                     </div>
 
-                    <article>
-                        <section>
-                            <img src="img/aboutPhoto.jpeg" width="100%"></img>
-                        </section>
-                    </article>
+                    <div class="justified">
 
-                    <aside>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio
-                            egestas tempor. Nunc
-                            vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper
-                            consequat est,
-                            eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam
-                            ullamcorper lorem
-                            dapibus velit suscipit ultrices.</p>
-                    </aside>
+                        <figure>
+                            <img src="img/mission.webp" width="100%"></img>
+                            <figcaption>
+                                Simon forging with Mateo C’ de Baca.<br/>
+                                Four day forging workshop at the Abbey of Regina Laudis, CT, USA. 11-17 August 2016
+                            <br/> 
+                                Simon forge avec Mateo C’ de Baca.<br/>
+                                Stage de ferronnerie de 4 jours à l’abbaye de Regina Laudis, Connecticut (USA). 11-17 août 2016
+                            </figcaption>
+                        </figure>
 
-                    <aside>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio
-                            egestas tempor. Nunc
-                            vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper
-                            consequat est,
-                            eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam
-                            ullamcorper lorem
-                            dapibus velit suscipit ultrices.</p>
-                    </aside>
-
+                        <p>
+                            <b>To enhance personal and public environments with unique creations in hand forged metals.</b>
+                        </p>
+                        <p>
+                            <b>Embellir les environnements publics et personnels avec des créations uniques en fer forgé à
+                                la main.</b>
+                        </p>
+                        <hr>
+                        <p>
+                            Internationally recognized as leading exponents in hand forging, forming stainless steel
+                            artworks either functional or purely aesthetic, for interiors, architecture and open spaces.
+                        </p>
+                        <p>
+                            Working closely with client, architect and contractor, their designs reflect the visions of
+                            their clients, providing endless joy for many years to come.
+                        </p>
+                        <p>
+                            Stainless steel today has many qualities, chiefly its sustainability and being maintenance
+                            free. Environmentally too, as most objects are made from recycled materials.
+                        </p>
+                        <p>
+                            Their work, as you will see within these pages, always has a foundation in aesthetics and
+                            proportion, coupled with well-proven engineering principles. The finished surface,
+                            reflecting the light and surrounding colors, makes the work appear lighter and more
+                            graceful, thus producing a totally different presence to any other metal.
+                        </p>
+                        <p> 
+                        <hr>
+                        <p>
+                            Reconnus de par le monde comme les représentants principaux dans l'art du fer forgé, créant
+                            des oeuvres fonctionnels ou esthétiques en inox pour des espaces en intérieur, en plein air,
+                            et en architecture.
+                        </p>
+                        <p>
+                            Travaillant auprès du client, de l'architecte et du contractant, leurs dessins reflètent la
+                            vision de leurs clients et donnent de la joie infinie pour des années à venir.
+                        </p>
+                        <p>
+                            Avec plus de 40 ans d'expérience et une famille qui remonte à plusieurs générations
+                            d'artisans, Forge Robinson est connue pour sa maîtrise de l'acier inoxydable.
+                        </p>
+                        <p>
+                            Ce métal aujourd'hui a beaucoup de qualités. En effet, l'inox est durable, ne demande aucun
+                            maintien, et s'accorde avec l'environnement; en effet, une grande partie des objets sont
+                            élaborés à partir de matériaux recyclés.
+                        </p>
+                        <p>
+                            Comme vous le verrez, ces oeuvres sont fondées dans l'esthétique et la proportion, et
+                            enracinées dans des principes solides de l'ingénierie. Une fois la surface finie, elle
+                            reflète la lumière et les couleurs de son entourage. L'oeuvre apparaît donc plus légère,
+                            plus gracieuse, et produit un effet et une présence uniques et introuvables chez d'autres
+                            métaux.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
