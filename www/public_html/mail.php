a5c0899d3f47c1c502c9ab194fce5744380ca68d
Unknown <richard.blake@hotmail.co.uk>
2020-08-26 22:09:07 +0100

    update gmail port

--- www/public_html/mail.php
+++ www/public_html/mail.php
@@ -22,7 +22,7 @@
   $mail->SMTPDebug  = 0;  
   $mail->SMTPAuth   = TRUE;
   $mail->SMTPSecure = "tls";
-  $mail->Port       = 25;
+  $mail->Port       = 587;
   $mail->Host       = "smtp.gmail.com";
   $mail->Username   = $configs['user'];
   $mail->Password   = $configs['pass'];
