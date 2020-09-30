16f319916b47161f10405994bfa3d1fa9bff9d0b
Unknown <richard.blake@hotmail.co.uk>
2020-09-30 21:15:44 +0100

    Add lang to navigation

--- www/public_html/header.php
+++ www/public_html/header.php
@@ -32,19 +32,19 @@
                     <li class="dropdown">
                         <a href="#">ABOUT <i class="arrow down"></i></a>
                         <div class="dropdown-content">
-                            <a href="/mission.php" class="navLink">MISSION</a>
-                            <a href="/simon.php" class="navLink">SIMON</a>
-                            <a href="/antony.php" class="navLink">ANTONY</a>
-                            <a href="/betsy.php" class="navLink">BETSY</a>
-                            <a href="/press.php" class="navLink">PRESS</a>
+                            <a href="/mission.php?lang=<?php echo$_GET['lang']?>" class="navLink">MISSION</a>
+                            <a href="/simon.php?lang=<?php echo$_GET['lang']?>" class="navLink">SIMON</a>
+                            <a href="/antony.php?lang=<?php echo$_GET['lang']?>" class="navLink">ANTONY</a>
+                            <a href="/betsy.php?lang=<?php echo$_GET['lang']?>" class="navLink">BETSY</a>
+                            <a href="/press.php?lang=<?php echo$_GET['lang']?>" class="navLink">PRESS</a>
                         </div>
                     </li>
                     <li><a href="/works/" class="navLink">WORKS</a></li>
                     <li class="dropdown">
                         <a href="#">APPROACH <i class="arrow down"></i></a>
                         <div class="dropdown-content">
-                            <a href="/process.php" class="navLink">PROCESS</a>
-                            <a href="/sketchbook.php" class="navLink">SKETCHBOOK</a>
+                            <a href="/process.php?lang=<?php echo$_GET['lang']?>" class="navLink">PROCESS</a>
+                            <a href="/sketchbook.php?lang=<?php echo$_GET['lang']?>" class="navLink">SKETCHBOOK</a>
                             <!--<a href="/challenges.php" class="navLink">CHALLENGES</a>-->
                         </div>
                     </li>
@@ -71,15 +71,18 @@
             }
         };
 
+        var _ddlLoaded = false;
+
         $('#lang-select').ddslick({
             width:70,
-            defaultSelectedIndex:0,
             onSelected: function(data){
 
                 console.log(data.selectedData.value);
                 console.log(getUrlParameter('lang'));
 
-                if(data.selectedData.value != getUrlParameter('lang')){
+                if(_ddlLoaded  === false) {
+                    _ddlLoaded = true;
+                } else if(data.selectedData.value != getUrlParameter('lang')){
                     var oldURL = window.location.href;
                     var newUrl = oldURL.split("?")[0] + "?lang=" + data.selectedData.value;
                     console.log(newUrl);
@@ -88,6 +91,8 @@
             }   
         });
 
+        let langIndex = getUrlParameter('lang') === 'en' ? 0 : 1;
+        $('#lang-select').ddslick('select', {index: langIndex });
 
     </script>
 
