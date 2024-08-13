4092a4de02a2eb0e33e5d36ebe1e7d20fce5f92b
Richard Blake <richard.blake@hotmail.co.uk>
2024-08-13 22:48:51 +0100

    Update galleryGenerator.php

--- www/public_html/galleryGenerator.php
+++ www/public_html/galleryGenerator.php
@@ -1,62 +1,28 @@
 <?php
 
-    use Google\Auth\ApplicationDefaultCredentials;
-    use GuzzleHttp\Client;
-    use GuzzleHttp\HandlerStack;
+    $imgFolder = '../img/' . $folderId . '/';
+    $images = glob($imgFolder . '*.{jpg}', GLOB_BRACE);
+    natsort($images);
 
-    use Google\Auth\Credentials\UserRefreshCredentials;
-    use Google\Photos\Library\V1\PhotosLibraryClient;
-    use Google\Photos\Library\V1\PhotosLibraryResourceFactory;
+    foreach ($images as $image) {
 
-    function cmp($a, $b) {
-        $order1 = intval(str_replace('.jpg', '', $a['title']));
-        $order2 = intval(str_replace('.jpg', '', $b['title']));
+        $imgTitle = explode(".",basename($image, ".jpg"))[1];
+        $imageDescription =  $imgTitle . " <br/><small>&copy; Simon Robinson " . date("Y") . " All rights reserved</small>";
+     
+ ?>
+     <div class="gallery-item" data-src="<?php echo $image; ?>"
+         style="background-image: url('<?php echo $image; ?>'), url('/img/loading.gif')"
+         data-sub-html="<?php echo $imageDescription; ?>"
+         data-pinterest-text="Work by Forge Robinson" data-tweet-text="Work by Forge Robinson"
+         data-facebook-text="Work by Forge Robinson">
+ 
+         <img src="<?php echo $image; ?>" hidden/>
+         
+     </div>
+ 
+ <?php 
+     } 
+ ?>
+ 
 
-        return $order1 > $order2;
-    }
 
-    // specify the path to your application credentials
-    if (file_exists('../../gapi-creds.json')) {
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=../../gapi-creds.json');
-    }else {
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=../gapi-creds.json');
-    }    
-
-    // define the scopes for your API call
-    $scopes = ['https://www.googleapis.com/auth/drive'];
-
-    // create middleware
-    $middleware = ApplicationDefaultCredentials::getMiddleware($scopes);
-    $stack = HandlerStack::create();
-    $stack->push($middleware);
-
-    // create the HTTP client
-    $client = new Client([
-        'handler' => $stack,
-        'base_uri' => 'https://www.googleapis.com',
-        'auth' => 'google_auth'  // authorize all requests
-    ]);
-
-    $responseFolder = $client->get('drive/v2/files?q=\'' . $folderId . '\'+in+parents');
-    $folderData = $responseFolder->getBody();
-    $driveFolder = json_decode($folderData, true);
-    $images = $driveFolder['items'];
-    usort($images, 'cmp'); 
-
-    foreach ($images as $imageFile) {
-        $imagePath = "https://drive.google.com/uc?id=" . $imageFile['id'];
-        $imageDescription = str_replace('"', '\'', $imageFile['description']) . " <br/><small>&copy; Simon Robinson " . date("Y") . " All rights reserved</small>";
-    
-?>
-    <div class="gallery-item" data-src="<?php echo $imagePath; ?>"
-        style="background-image: url('<?php echo $imagePath; ?>'), url('/img/loading.gif')"
-        data-sub-html="<?php echo $imageDescription; ?>"
-        data-pinterest-text="Work by Forge Robinson" data-tweet-text="Work by Forge Robinson"
-        data-facebook-text="Work by Forge Robinson">
-
-        <img src="<?php echo $imagePath; ?>" hidden/>
-    </div>
-
-<?php 
-    } 
-?>
